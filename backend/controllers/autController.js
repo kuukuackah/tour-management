8d7d8fe2fd289c80f3253191bc4d32071f1855af
kuukuackah <98889348+kuukuackah@users.noreply.github.com>
2023-06-26 13:56:20 +0000

    new changes

--- backend/controllers/autController.js
+++ backend/controllers/autController.js
@@ -1,21 +1,34 @@
-
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // user registration
-export const register =async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}; 
+export const register = async (req, res) => {
+  try {
+    // hashing password
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-//user login
-export const logi =async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+      photo: req.body.photo,
+    });
+
+    await newUser.save();
+
+    res.status(200).json({ success: true, message: "Successfully created" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to create. Please try again." });
+  }
 };
 
+// user login
+export const login = async (req, res) => {
+  try {
+    // Add code for user login
+  } catch (error) {
+    // Handle error
+  }
+};
